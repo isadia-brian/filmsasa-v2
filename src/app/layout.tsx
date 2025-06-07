d596ac69eb8d26b890845bd171c64e5fd5489e60
isadia-brian <developerisadia@gmail.com>
2025-06-08 00:46:23 +0300

    feat: added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-// import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${poppins.className} antialiased bg-neutral-900`}>
         {children}
-        {/*<Analytics />*/}
+        <Analytics />
         <Toaster />
       </body>
     </html>
